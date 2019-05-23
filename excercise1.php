49e66329a8bf57e1198945dab74c81e8e0418262
ThaisanUN <mr.thaisan1213@gmail.com>
2019-05-23 12:05:55 +0700

    lab1

--- excercise1.php
+++ excercise1.php
@@ -1,3 +1,22 @@
 <?php
-$arr =[a,b,c,d];
+$arr = range('A','Z');
+$str = "khoor";
+$shitf = 3;
+$result = "";
+$str = strtoupper($str);
+
+for($i = 0; $i < strlen($str); $i++){
+  if(in_array($str[$i], $arr)){
+    $index = array_search($str[$i], $arr);
+    $key = ($index - $shitf) % 26;
+    $result .= $arr[$key];
+
+
+  }
+  else{
+    $result .= $str[$i];
+  }
+}
+echo $result;
+
 ?>
